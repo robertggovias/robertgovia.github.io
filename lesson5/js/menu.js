b1f7b36a53d4ee8565147b6879df036b2a48c132
robertggovias <goviasuarez@gmail.com>
2019-10-20 03:55:18 -0200

    update

--- lesson5/js/menu.js
+++ lesson5/js/menu.js
@@ -1,3 +1,6 @@
 function toggleMenu(){
     document.getElementsByClassName("menu")[0].classList.toggle("responsive");
 }
+function toggleAd(){
+    document.getElementsByClassName("announce")[0].classList.toggle("adout");
+}
