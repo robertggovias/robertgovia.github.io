15419e6421f60d12d8ff912d4121937394f37eaf
robertggovias <goviasuarez@gmail.com>
2019-10-23 07:23:07 -0200

    update

--- lesson5/js/panqueke.js
+++ lesson5/js/panqueke.js
@@ -1,13 +1,4 @@
-if(new Date().getDay() == 4){
+if(new Date().getDay() != 5){
     document.getElementById("announce").style.display = "none";}
     else{
-        document.getElementById("announce").style.color = "blue";}
-
-/*var d = new Date();
-var n = d.getDay();
-if(n == 2){
-		document.getElementsById("announce").style.color = "red";
-	  }
-	  else{
-		document.getElementsById("announce").style.display = "blue";
-	  }*/
+        document.getElementById("announce").style.color = "#1394f6";}
