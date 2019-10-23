654f101e4067ffa87c53f25412b460de70fa675f
robertggovias <goviasuarez@gmail.com>
2019-10-23 07:32:18 -0200

    update

--- lesson5/js/panqueke.js
+++ lesson5/js/panqueke.js
@@ -1,4 +1,4 @@
-if(new Date().getDay() != 5){
+if(new Date().getDay() != 3){
     document.getElementById("announce").style.display = "none";}
     else{
         document.getElementById("announce").style.color = "#1394f6";}
