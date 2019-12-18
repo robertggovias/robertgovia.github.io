5632539716fd5311ee6343a9377ec1817d4325eb
robertggovias <goviasuarez@gmail.com>
2019-12-18 09:04:13 -0300

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,10 +1,17 @@
-const requestURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={89bc7a863b25584dbe9c64dc38e40845}';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=89bc7a863b25584dbe9c64dc38e40845';
 
-fconst apiURL = "...";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+    //icons
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
   });
     // loop through every record and process them into their own 'cards' (HTML output)
   /*for (let i = 0; i < towns.length; i++ ) {      
