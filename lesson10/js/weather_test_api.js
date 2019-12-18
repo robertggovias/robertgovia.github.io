930234dcd5684545787e95a9b02ee4cf59725388
robertggovias <goviasuarez@gmail.com>
2019-12-18 15:12:43 -0300

    update

--- lesson10/js/weather_test_api.js
+++ lesson10/js/weather_test_api.js
@@ -1,47 +1,20 @@
-/*const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=89bc7a863b25584dbe9c64dc38e40845';
-
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    for (let i = 0; i < jsObject.list.length; i++) {
-      if (jsObject.list[i].dt_txt.includes('18:00:00')) {
-        console.log(jsObject.list[i].dt_txt);
-      }
-    }
-  });*/
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=89bc7a863b25584dbe9c64dc38e40845';
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     document.getElementById('current-temp').textContent = jsObject.main.temp;
-    document.getElementById('current').textContent = jsObject.weather[0].description;
-    document.getElementById('humid').textContent = jsObject.main.humidity;
-    document.getElementById('wind').textContent = jsObject.wind.speed;
-    
-    
 
     //icons
 
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
-//document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
   });
-
-
-
-
-
-
-
-
-
-
-/*// ---------------------------------------------------------loop through every record and process them into their own 'cards' (HTML output)
-for (let i = 0; i < towns.length; i++ ) {      
+    // loop through every record and process them into their own 'cards' (HTML output)
+  /*for (let i = 0; i < towns.length; i++ ) {      
     if(towns[i].name=="Preston" || towns[i].name=="Soda Springs" || towns[i].name=="Fish Haven"){    
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
@@ -58,5 +31,4 @@ for (let i = 0; i < towns.length; i++ ) {
     document.querySelector('div.cards').appendChild(card);
     
 }}
-  });
-  */
+  });*/
