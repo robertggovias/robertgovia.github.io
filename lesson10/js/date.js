459e2b1df646b53acf9fc41914aa4fb9f17fe748
robertggovias <goviasuarez@gmail.com>
2019-12-18 20:11:05 -0300

    update

--- lesson10/js/date.js
+++ lesson10/js/date.js
@@ -5,7 +5,15 @@ let daynames = [
 	"Wednesday",
 	"Thursday",
 	"Friday",
-	"Saturday"
+	"Saturday",
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+	
 ];
 let months = [
 	"January",
@@ -28,8 +36,28 @@ let year = d.getFullYear();
 let hour = d.getHours();
 let minutes = d.getMinutes();
 let seconds = d.getSeconds();
-let fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
 document.getElementById("currentdate").textContent = fulldate;
+if(d.getDay()==5){document.getElementById("announce").textContent = "Saturday at 9:00 a.m. Preston Pancakes in the Park! at the city park pavilion."};
+var ddd, ddn, dd;
+ddn = 0;
+for (dd = 0; dd < 12; dd++) {
+	if (hour > 18) {
+		ddd = "d" + ddn;
+		document.getElementById(ddd).textContent = daynames[d.getDay() + dd + 1];
+		ddn += 1;
+	} else {
+		ddd = "d" + ddn;
+		document.getElementById(ddd).textContent = daynames[d.getDay() + dd];
+		ddn += 1;
+	}
+}
+//
+    
 // ****************************
-  let options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  
+let options = {
+	weekday: 'long',
+	day: 'numeric',
+	month: 'long',
+	year: 'numeric'
+};
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -10,47 +10,35 @@ fetch(apiURL)
     document.getElementById('wind').textContent = jsObject.wind.speed;
     //icons
 
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-//document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+   
   });
   
   //5 daays temperature
   const apifiveURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=89bc7a863b25584dbe9c64dc38e40845';
-var ic, n, t, count;
+var ic, n, t, count,icp,j,k, mic, mn, mt, count,icp,j,k;
 n=0;
 ic=[];
 t=[];
-count=[];
-
+icp=[];
 fetch(apifiveURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    for (let i = 0; i < jsObject.list.length; i++) {
-      document.getElementById("t3").innerHTML = i;
+    for (let i = 0; i < jsObject.list.length; i++) {      
       if (jsObject.list[i].dt_txt.includes('18:00:00')) {
-        t += jsObject.list[i].main.temp;
-        document.getElementById("t0").innerHTML = t[0]+t[1]+t[2]+t[3];
-        document.getElementById("t1").innerHTML = t[5]+t[6]+t[7]+t[8];
-        document.getElementById("t2").innerHTML = t[10]+t[11]+t[12]+t[13];
-        document.getElementById("t3").innerHTML = t[15]+t[16]+t[17]+t[18];
-        document.getElementById("t4").innerHTML = t[20]+t[21]+t[22]+t[19]; 
-        ic[n]= jsObject.list[i].weather[0].icon;
-        count[n]=n;
+        t[n]= jsObject.list[i].main.temp.toFixed(0);        
+        k="t"+n;
+        document.getElementById(k).textContent = t[n];       
+        ic[n]= jsObject.list[i].weather[0].icon;        
+        icp[n]= "https://openweathermap.org/img/w/" + ic[n] + ".png";        
+        j="i"+n;
+        document.getElementById(j).setAttribute('src', icp[n]);
         n+=1;
-        
-        document.getElementById("i0").innerHTML = ic[0];
-        document.getElementById("i1").innerHTML = ic[1];
-        document.getElementById("i2").innerHTML = ic[2];
-        document.getElementById("i3").innerHTML = ic[3];
-        document.getElementById("i4").innerHTML = ic[4];
         }
     }
     
-
+    
   });
+  
  
   
