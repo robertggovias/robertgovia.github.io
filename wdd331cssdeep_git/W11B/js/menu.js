b4cf260c096359ba3fa6141322c3a065b18d43d2
robertggovias <goviasuarez@gmail.com>
2020-03-22 11:44:51 -0300

    update

---
+++ wdd331cssdeep_git/W11B/js/menu.js
@@ -0,0 +1,11 @@
+function toggleNav() {
+   var updateElement = document.getElementById("b-body");    
+ updateElement.classList.toggle("body-open");
+    var updateElement = document.getElementById("prueba");    
+ updateElement.classList.toggle("prueba-open");
+     var updateElement = document.getElementById("menu-icon");    
+ updateElement.classList.toggle("open");
+    
+       
+    }
+    
