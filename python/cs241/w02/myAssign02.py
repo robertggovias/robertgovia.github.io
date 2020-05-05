a9faae6eb552946e300843ced484bf24693cf7b0
robertggovias <goviasuarez@gmail.com>
2020-05-05 08:17:12 -0300

    update

--- python/cs241/w02/myAssign02.py
+++ python/cs241/w02/myAssign02.py
@@ -3,12 +3,23 @@ def prompt_file():
     return x
 def parse(x):
     y=open(x,"r")
-    p=y.readlines()    
+    p=y.readlines()
     return p
+def printLine(x,y,z):
+    f=print(x[y][z])
+    return f
+
+def splitPrint(x,y):    
+    for i in x[y]:
+        z = i.split(",")
+    e=print(z)
+    #f=print("{} ({}, {}) - ${}".format(z[2],z[0],z[3],z[6]))
+    return e
 
 def main():
     x = parse(prompt_file())
-    print(x[3])
-    return x
+    y = printLine(x,1,2)
+    #y = splitPrint(x,4)
+    return y
 if __name__ == "__main__":
     main()
