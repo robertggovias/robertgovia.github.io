58581b26fd80afbf719071424e0712aa9cc645fc
robertggovias <goviasuarez@gmail.com>
2020-10-01 13:30:59 -0300

    update

--- python/cs241/w03/testando funciones.py
+++ python/cs241/w03/testando funciones.py
@@ -1,17 +1,20 @@
 class Point:
     def __init__(self, pX,pY):
-        self.x = pY
+        self.x = pX
         self.y = pY
     def gX(self):
         return self.x
     
     def gY(self):
         return self.y
+    def distance(self):
+        return ((self.x ** 2)+(self.y**2))**0.5
+
+def distance(point1, point2):
+    xdiference = p2.gX() - p1.gX()
+    ydiference = 
         
 p = Point(4,5)
 print(p.gX())
 print(p.gY())
 
-p2 = Point(1,2)
-print(p2.gX())
-print(p2.gY())
