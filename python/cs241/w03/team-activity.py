00313613227ad900d3951eec27990e0a4ea33de5
robertggovias <goviasuarez@gmail.com>
2020-10-13 12:45:09 -0300

    update

--- python/cs241/w03/team-activity.py
+++ python/cs241/w03/team-activity.py
@@ -1,36 +1,37 @@
 class Complex:
-    def __init__(self, numerator, denominator):
+
+
+    '''def __init__(self, numerator, denominator):
         self.n = numerator
-        self.d = denominator
+        self.d = denominator'''
     
+    def __init__(self):
+        self.n = 0
+        self.d = 0
+
+
     def display(self):
-        print(str(self.n) + "/" + str(self.d))
+        print ("{}/{}".format(self.n, self.d))
+        #print(str(self.n) + "/" + str(self.d))
     
     def prompt(self):
         self.n = input("Enter the numerator: ")
         self.d = input("Enter the denominator: ")
     
     def display_decimal(self):
-        return int(self.n / self.d)
+        f = int(self.n) / int(self.d)
+        print(f)
         
 def main():
-    """
-    This function tests your Complex class. It should have a prompt
-    and a display member function to be called.
-
-    You should not need to change this main function at all.
-    """
-    c1 = Complex(0,1)    
-    c2 = Complex(0,1)    
-        
+    
+    c1 = Complex()
+    #c1 = Complex(0,1)    
     c1.display()
+    c1.prompt()
+    
+    c1.display_decimal()
 
-    print()
-    c2.prompt()
-
-    c2.display()
-
-    c2.display_decimal()
+    
 
 # If this is the main program being run, call our main function above
 if __name__ == "__main__":
