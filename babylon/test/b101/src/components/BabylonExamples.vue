0427d5e3c31feb52f4f25324de125a5de2a9afbe
robertggovias <goviasuarez@gmail.com>
2022-07-26 06:59:41 -0300

    update

---
+++ babylon/test/b101/src/components/BabylonExamples.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+  <h3>first moon step for Expoverse</h3>
+
+  <canvas></canvas>
+ </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { StandarMaterials } from '@/BabylonExamples/StandarMaterials'; 
+
+
+export default defineComponent({
+  name: 'BabylonExamples',
+  mounted(){
+    const canvas = document.querySelector("canvas")!;
+    new StandarMaterials(canvas);
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+canvas{
+  width: 80%;
+  height: 70%;
+
+}
+</style>
