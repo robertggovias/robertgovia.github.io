a0676aa5498f8e1c22df321cf7e95fc5d8d6148e
robertggovias <goviasuarez@gmail.com>
2023-04-02 19:44:35 -0300

    update

---
+++ python/automate/materiales-rinho.py
@@ -0,0 +1,35 @@
+import pyautogui
+import time
+#nombre
+pyautogui.hotkey("Alt","Tab")
+ynome = 265
+ytriggernome = 265
+ywritenome = 200
+ycolor = 289
+suma = 19
+def createName():    
+    
+    pyautogui.moveTo(x=1139, y=ynome)
+    pyautogui.doubleClick(x=1139, y=ynome)
+    pyautogui.click(x=1139, y=ynome)
+    pyautogui.hotkey("ctrl","a")    
+    pyautogui.hotkey("Ctrl","c")
+    pyautogui.moveTo(x=1315, y=ytriggernome)
+    pyautogui.click(x=1315, y=ytriggernome)
+    pyautogui.moveTo(x=550, y=243)
+    pyautogui.click(x=550, y=243)   
+    pyautogui.hotkey("ctrl","a")    
+    pyautogui.press("delete") 
+    pyautogui.hotkey("Ctrl","v")
+    #ok nome    
+    pyautogui.click(x=687, y=756)
+
+
+
+
+for i in range(10):
+    createName()  
+    ynome = ynome + suma
+    ytriggernome = ytriggernome + suma
+    ywritenome = ywritenome + suma
+    ycolor = ycolor + suma
